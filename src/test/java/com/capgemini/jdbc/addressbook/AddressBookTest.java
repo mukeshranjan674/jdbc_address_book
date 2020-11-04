b43fb61a1e9902cc6632b8fe5b17aa17de12f67a
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-11-04 18:20:07 +0530

    [Mukesh] add : test files written

---
+++ src/test/java/com/capgemini/jdbc/addressbook/AddressBookTest.java
@@ -0,0 +1,20 @@
+package com.capgemini.jdbc.addressbook;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AddressBookTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
